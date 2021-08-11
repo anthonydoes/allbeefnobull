a87fffcab8823606eb2ce65ef2399697d42776cd
anthonydoes <anthonypyannos@gmail.com>
2021-08-10 20:22:23 -0400

    add loading screen

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+$('body').append('<div style="" id="loadingDiv" class="loadingContainer"><div class="loader">Loading...</div></div>');
+$(window).on('load', function(){
+  setTimeout(removeLoader, 1800); //wait for page load PLUS two seconds.
+});
+function removeLoader(){
+    $( "#loadingDiv" ).fadeOut(500, function() {
+      // fadeOut complete. Remove the loading div
+      $( "#loadingDiv" ).remove(); //makes page more lightweight 
+  });  
+}
 
 // Menu toggle
 
